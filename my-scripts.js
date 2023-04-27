79f85cf432fd3ff915f191d3f9600f122147d89e
GeorgeKlemperer <george@klemperer.co.uk>
2023-04-27 11:48:43 +0100

    Basic website v1

---
+++ my-scripts.js
@@ -0,0 +1,3 @@
+const date = new Date();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("date").innerHTML = days[date.getDay()]+" "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
